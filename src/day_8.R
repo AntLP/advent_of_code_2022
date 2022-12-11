a1146727de665c9d76fb25006103483719fc8515
Antoine Lachance-Perreault <antoine.lach@gmail.com>
2022-12-11 11:47:36 -0500

    days 8-9

--- src/day_8.R
+++ src/day_8.R
@@ -4,3 +4,149 @@ library(tidyverse)
 input_test <- readLines("./data/day_8_test.txt")
 input <- readLines("./data/day_8_input.txt")
 
+process_input <- function(input){
+  input %>% 
+    str_split("", simplify = T) %>% 
+    apply(c(1, 2), as.numeric)
+  
+}
+
+is_tallest <- function(vector){
+  # Tree is visible if
+  #   It is the tallest from the given direction and
+  #   the second tallest is strictly smaller
+  return(vector[1] == max(vector) & length(vector[vector == max(vector)]) == 1)
+  
+}
+
+is_visible_vector <- function(id, tree_vector){
+  dir1 <- tree_vector[1:id]
+  dir2 <- tree_vector[id:length(tree_vector)]
+  
+  return(is_tallest(rev(dir1))|is_tallest(dir2))
+}
+
+
+is_visible <- function(row, col, tree_matrix){
+  tree_row <- tree_matrix[row,]
+  tree_col <- tree_matrix[,col]
+  
+  return(is_visible_vector(col, tree_row)|is_visible_vector(row, tree_col))
+}
+
+
+determine_visible <- function(input){
+  processed_input <- process_input(input)
+  
+  vis_matrix <- processed_input
+  
+  for(i in 1:nrow(processed_input)){
+    for(j in 1:ncol(processed_input)){
+      vis_matrix[i, j] <- is_visible(i, j, processed_input)
+    }
+  }
+  
+  vis_matrix
+  
+}
+
+count_visible_tree <- function(input){
+  determine_visible(input) %>% 
+    sum()
+}
+
+
+assertthat::are_equal(
+  count_visible_tree(input_test),
+  21
+)
+
+
+
+
+count_visible_tree(input)
+
+
+
+# part 2 ----
+
+
+calc_scen_dist <- function(tree_height, vec_from_tree){
+  size_comp <- vec_from_tree < tree_height
+  return(min(sum(cumprod(size_comp)) + 1, length(vec_from_tree)))
+}
+
+scen_dist_vect <- function(id, tree_vector){
+  
+  if(id == 1 | id == length(tree_vector)){
+    return(0)
+  }
+  
+  dir1 <- rev(tree_vector[1:(id - 1)])
+  dir2 <- tree_vector[(id + 1):length(tree_vector)]
+  
+  return(c(calc_scen_dist(tree_vector[id], dir1), calc_scen_dist(tree_vector[id], dir2)))
+}
+
+
+scen_dist <- function(row, col, tree_matrix){
+  tree_row <- tree_matrix[row,]
+  tree_col <- tree_matrix[,col]
+  
+  return(prod(scen_dist_vect(col, tree_row), scen_dist_vect(row, tree_col)))
+}
+
+
+
+
+calc_all_scen_dit <- function(input){
+  processed_input <- process_input(input)
+  
+  vis_matrix <- processed_input
+  
+  for(i in 1:nrow(processed_input)){
+    for(j in 1:ncol(processed_input)){
+      vis_matrix[i, j] <- scen_dist(i, j, processed_input)
+    }
+  }
+  
+  vis_matrix
+  
+}
+
+maximum_scen_dist <- function(input){
+  calc_all_scen_dit(input) %>% 
+    max()
+}
+
+
+
+
+assertthat::are_equal(
+  maximum_scen_dist(input_test),
+  8
+)
+
+maximum_scen_dist(input)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/day_9.R
+++ src/day_9.R
@@ -4,3 +4,182 @@ library(tidyverse)
 input_test <- readLines("./data/day_9_test.txt")
 input <- readLines("./data/day_9_input.txt")
 
+
+print_matrix <- function(curr_head, curr_tail){
+  mat <- matrix(nrow = 5, ncol = 6, data = rep("", 30))
+  
+  mat[5 - curr_head[1], curr_head[2] + 1] <- "H"
+  mat[5 - curr_tail[1], curr_tail[2] + 1] <- "T"
+  
+  print(mat)
+}
+
+update_positions <- function(dir, n, curr_head, curr_tail){
+  
+  visited_positions <- c()
+  
+  for(i in 1:n){
+    curr_head <- update_head_position(dir, curr_head)
+    curr_tail <- update_tail_position(curr_head, curr_tail)
+    visited_positions <- c(visited_positions, paste(curr_tail, collapse = "x"))
+    
+    # print_matrix(curr_head, curr_tail)
+  }
+  
+  return(list(
+    curr_head = curr_head,
+    curr_tail = curr_tail,
+    visited_positions = visited_positions
+  ))
+}
+
+
+update_head_position <- function(dir, curr_head){
+  dir_add <- case_when(
+    dir == "U" ~ c(1, 0),
+    dir == "D" ~ c(-1, 0),
+    dir == "R" ~ c(0, 1),
+    dir == "L" ~ c(0, -1)
+  )
+  
+  # return(pmax(pmin(curr_head + dir_add, 5), 0))
+  return(curr_head + dir_add)
+}
+
+
+update_tail_position <- function(curr_head, curr_tail){
+  
+  if(max(abs(curr_head - curr_tail)) < 2){
+    new_position <- curr_tail
+  } else {
+    new_position <- mapply(
+      function(x, y){ifelse(y >= 0, ceiling(x), floor(x))}, 
+      x = (curr_head + curr_tail) / 2, 
+      y = sign(curr_head - curr_tail)
+    )
+  }
+  
+  
+  new_position
+}
+
+
+process_input <- function(instructions){
+  tibble(instructions = instructions) %>% 
+    separate(instructions, c("dir", "n"), sep = " ") %>% 
+    mutate(n = as.integer(n))
+}
+
+
+count_all_positions <- function(instructions){
+  
+  curr_head <- c(0, 0)
+  curr_tail <- c(0, 0)
+  visited_positions <- c("0x0")
+  
+  instructions <- process_input(instructions)
+  
+  for(i in 1:nrow(instructions)){
+    # print(instructions$dir[i])
+    new_positions <- update_positions(instructions$dir[i], instructions$n[i], curr_head, curr_tail)
+    
+    curr_head <- new_positions$curr_head
+    curr_tail <- new_positions$curr_tail
+    visited_positions <- c(visited_positions, new_positions$visited_positions)
+  }
+  
+  
+  return(length(unique(visited_positions)))
+}
+
+assertthat::are_equal(
+  count_all_positions(input_test),
+  13
+)
+
+
+count_all_positions(input)
+
+
+
+
+
+
+
+
+
+
+# part 2 ----
+
+
+
+update_positions_10 <- function(dir, n, curr_positions){
+  
+  visited_positions <- c()
+  
+  for(i in 1:n){
+    curr_positions$head <- update_head_position(dir, curr_positions$head)
+    curr_positions$p1 <- update_tail_position(curr_positions$head, curr_positions$p1)
+    curr_positions$p2 <- update_tail_position(curr_positions$p1, curr_positions$p2)
+    curr_positions$p3 <- update_tail_position(curr_positions$p2, curr_positions$p3)
+    curr_positions$p4 <- update_tail_position(curr_positions$p3, curr_positions$p4)
+    curr_positions$p5 <- update_tail_position(curr_positions$p4, curr_positions$p5)
+    curr_positions$p6 <- update_tail_position(curr_positions$p5, curr_positions$p6)
+    curr_positions$p7 <- update_tail_position(curr_positions$p6, curr_positions$p7)
+    curr_positions$p8 <- update_tail_position(curr_positions$p7, curr_positions$p8)
+    curr_positions$tail <- update_tail_position(curr_positions$p8, curr_positions$tail)
+    
+    visited_positions <- c(visited_positions, paste(curr_positions$tail, collapse = "x"))
+    
+  }
+  
+  return(list(
+    curr_positions = curr_positions,
+    visited_positions = visited_positions
+  ))
+}
+
+
+count_all_positions_10 <- function(instructions){
+  
+  curr_positions <- list()
+  
+  curr_positions$head <- c(0, 0)
+  curr_positions$p1 <- c(0, 0)
+  curr_positions$p2 <- c(0, 0)
+  curr_positions$p3 <- c(0, 0)
+  curr_positions$p4 <- c(0, 0)
+  curr_positions$p5 <- c(0, 0)
+  curr_positions$p6 <- c(0, 0)
+  curr_positions$p7 <- c(0, 0)
+  curr_positions$p8 <- c(0, 0)
+  curr_positions$tail <- c(0, 0)
+  
+  visited_positions <- c("0x0")
+  
+  instructions <- process_input(instructions)
+  
+  for(i in 1:nrow(instructions)){
+    new_positions <- update_positions_10(instructions$dir[i], instructions$n[i], curr_positions)
+    
+    curr_positions <- new_positions$curr_positions
+    visited_positions <- c(visited_positions, new_positions$visited_positions)
+    
+  }
+  
+  
+  return(length(unique(visited_positions)))
+}
+
+
+
+input_test2 <- readLines("./data/day_9_test2.txt")
+
+assertthat::are_equal(
+  count_all_positions_10(input_test2),
+  36
+)
+
+count_all_positions_10(input)
+
+
